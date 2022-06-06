fbb9444297567b7e75d0cd0b41fcdfd5725359f9
ashwini <ashwinidivakar8@gmail.com>
2022-06-06 11:43:54 +0100

    connecting to database

---
+++ src/main/java/com/nology/cloudtravelapp/Locations.java
@@ -0,0 +1,54 @@
+package com.nology.cloudtravelapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+
+@Entity
+public class Locations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int locationId;
+    private String locationName;
+    private String description;
+    private ArrayList<String> attractions;
+
+    public Locations(){
+
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getAttractions() {
+        return attractions;
+    }
+
+    public void setAttractions(ArrayList<String> attractions) {
+        this.attractions = attractions;
+    }
+}
+
--- src/main/java/com/nology/cloudtravelapp/TravelController.java
+++ src/main/java/com/nology/cloudtravelapp/TravelController.java
@@ -1,13 +1,31 @@
 package com.nology.cloudtravelapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TravelController {
+
+    @Autowired
+    LocationRepository locationRepository;
 
     @GetMapping("/location")
     public String getTravelDetails(){
         return "getting location";
     }
+
+    @GetMapping("/locations")
+    public List<Locations> getLocations(){
+        return locationRepository.findAll();
+    }
+//
+//    public String addLocation(@RequestBody Locations text){
+//        this.locationRepository.save(text);
+//        System.out.println("Added"+text);
+//        return "Location saved";
+//    }
 }
