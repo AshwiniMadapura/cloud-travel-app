b36ee97c611478503186d831bbfd4b309ab2bc9d
ashwini <ashwinidivakar8@gmail.com>
2022-06-09 16:26:56 +0100

    changed CrossOrigin origin to cloud db

--- src/main/java/com/nology/cloudtravelapp/TravelController.java
+++ src/main/java/com/nology/cloudtravelapp/TravelController.java
@@ -7,7 +7,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"https://cloud-travel-app-mgpd7hiizq-nw.a.run.app/"})
 public class TravelController {
 
     @Autowired
